d2060c5c589c248e270908cef64bd45efb23d1a2
MarcoTz <marco.tzschentke@gmail.com>
2024-12-06 13:17:56 +0100

    added tests fo generate prelude and conclusion

--- chapter2/src/x86_int/prelude_conclusion.rs
+++ chapter2/src/x86_int/prelude_conclusion.rs
@@ -36,3 +36,54 @@ pub fn generate_prelude_conclusion(prog: Program) -> Program {
         global_labels,
     }
 }
+
+#[cfg(test)]
+mod prelude_conclusion_tests {
+    use super::{generate_prelude_conclusion, Arg, Instr, Program, Reg};
+    use std::collections::{HashMap, HashSet};
+
+    #[test]
+    fn gen_prelude_conc() {
+        let result = generate_prelude_conclusion(Program {
+            blocks: HashMap::from([(
+                "start".to_owned(),
+                vec![Instr::AddQ(Arg::Immediate(10), Arg::Immediate(5))],
+            )]),
+            stack_space: 0,
+            global_labels: HashSet::new(),
+        });
+        let labels = if std::env::consts::OS == "macos" {
+            ["_main", "_start", "_conclusion"]
+        } else {
+            ["main", "start", "conclusion"]
+        };
+        let expected = Program {
+            blocks: HashMap::from([
+                (
+                    labels[0].to_owned(),
+                    vec![
+                        Instr::PushQ(Arg::Reg(Reg::Rbp)),
+                        Instr::MovQ(Arg::Reg(Reg::Rsp), Arg::Reg(Reg::Rbp)),
+                        Instr::SubQ(Arg::Immediate(16), Arg::Reg(Reg::Rsp)),
+                        Instr::Jump("start".to_owned()),
+                    ],
+                ),
+                (
+                    labels[1].to_owned(),
+                    vec![Instr::AddQ(Arg::Immediate(10), Arg::Immediate(5))],
+                ),
+                (
+                    labels[2].to_owned(),
+                    vec![
+                        Instr::AddQ(Arg::Immediate(16), Arg::Reg(Reg::Rsp)),
+                        Instr::PopQ(Arg::Reg(Reg::Rbp)),
+                        Instr::RetQ,
+                    ],
+                ),
+            ]),
+            stack_space: 0,
+            global_labels: HashSet::from([labels[0].to_owned()]),
+        };
+        assert_eq!(result, expected)
+    }
+}
