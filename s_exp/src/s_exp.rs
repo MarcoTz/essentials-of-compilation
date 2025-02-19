8dac420b0f2c168889e0783047389538fd604162
MarcoTz <marco.tzschentke@gmail.com>
2025-02-19 17:16:42 +0100

    fixed s expression parser

--- s_exp/src/s_exp.rs
+++ s_exp/src/s_exp.rs
@@ -4,23 +4,21 @@ use std::{fmt, str::FromStr};
 #[derive(Debug, PartialEq, Eq)]
 pub enum SExp {
     Symbol(String),
-    Expr(Vec<SExp>),
+    Expr(String, Box<SExp>),
 }
 
 impl FromStr for SExp {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if !s.contains(" ") {
-            let s = if s.chars().next() == Some('(') && s.chars().last() == Some(')') {
-                let rest = s.split_at(0).1;
-                rest.split_at(rest.len() - 1).0
+        if s.chars().next() != Some('(') {
+            if s.contains(" ") || s.contains("(") {
+                return Err(Error::MissingParens);
             } else {
-                s
-            };
-            return Ok(SExp::Symbol(s.to_owned()));
+                return Ok(SExp::Symbol(s.to_owned()));
+            }
         }
 
-        let (parens_o, rest) = s.split_at(0);
+        let (parens_o, rest) = s.split_at(1);
         if parens_o != "(" {
             return Err(Error::MissingParens);
         }
@@ -30,12 +28,13 @@ impl FromStr for SExp {
             return Err(Error::MissingParens);
         }
 
-        let expr_strs: Vec<&str> = rest.split(" ").collect();
-        let inner_exps = expr_strs
-            .into_iter()
-            .map(|s| s.parse::<SExp>())
-            .collect::<Result<Vec<SExp>, Self::Err>>()?;
-        Ok(SExp::Expr(inner_exps))
+        let next_split = match rest.find("(") {
+            None => return Ok(SExp::Symbol(rest.to_owned())),
+            Some(ind) => ind,
+        };
+        let (fst, rest) = rest.split_at(next_split);
+        let rest_parsed = rest.parse::<SExp>()?;
+        Ok(SExp::Expr(fst.trim().to_owned(), Box::new(rest_parsed)))
     }
 }
 
@@ -43,14 +42,7 @@ impl fmt::Display for SExp {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             SExp::Symbol(c) => write!(f, "({c})"),
-            SExp::Expr(exprs) => {
-                let expr_str = exprs
-                    .iter()
-                    .map(|e| e.to_string())
-                    .collect::<Vec<String>>()
-                    .join(" ");
-                write!(f, "({expr_str})")
-            }
+            SExp::Expr(fst, rst) => write!(f, "({fst} {rst})"),
         }
     }
 }
@@ -67,5 +59,22 @@ mod s_exp_tests {
     }
 
     #[test]
-    fn parse_list() {}
+    fn parse_parens() {
+        let result = "(Nil)".parse::<SExp>().unwrap();
+        let expected = SExp::Symbol("Nil".to_owned());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn parse_list() {
+        let result = "(1 (2 (Nil)))".parse::<SExp>().unwrap();
+        let expected = SExp::Expr(
+            "1".to_owned(),
+            Box::new(SExp::Expr(
+                "2".to_owned(),
+                Box::new(SExp::Symbol("Nil".to_owned())),
+            )),
+        );
+        assert_eq!(result, expected)
+    }
 }
