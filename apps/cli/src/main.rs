15362a546b0e763a1a772142659a0122df5c711e
MarcoTz <marco.tzschentke@gmail.com>
2025-08-23 13:33:00 +0200

    added tests

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -1,15 +1,3 @@
-use parser::parse_program;
-use std::fs::{read_dir, read_to_string};
-
-const EXAMPLES_DIR: &str = "examples";
-
 fn main() {
-    let dir_contents = read_dir(EXAMPLES_DIR).unwrap();
-    for entry in dir_contents {
-        let entry = entry.unwrap();
-        let example = read_to_string(entry.path()).unwrap();
-        println!("parsing {:?}", entry.path());
-        let parsed = parse_program(&example).unwrap();
-        println!("{parsed:?}");
-    }
+    todo!()
 }
