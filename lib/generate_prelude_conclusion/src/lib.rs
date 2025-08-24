b27e195d8ebd71a7555514d7afe7d03bab03fd23
MarcoTz <marco.tzschentke@gmail.com>
2025-08-24 17:28:27 +0200

    removed macos specific labels and added return code

--- lib/generate_prelude_conclusion/src/lib.rs
+++ lib/generate_prelude_conclusion/src/lib.rs
@@ -1,30 +1,20 @@
-use std::env::consts;
 use syntax::x86::{Arg, Instruction, Program, Reg};
 
 pub fn generate_prelude_conclusion(prog: Program) -> Program {
-    let prelude = generate_prelude(&prog)
-        .into_iter()
-        .map(|instr| patch_label_instr(instr))
-        .collect();
-    let conclusion = generate_conclusion(&prog)
-        .into_iter()
-        .map(|instr| patch_label_instr(instr))
-        .collect();
+    let prelude = generate_prelude(&prog);
+    let conclusion = generate_conclusion(&prog);
     let mut finalized = Program::new(prog.stack_space, prog.used_callee);
-    finalized.add_block(&patch_label("main"), prelude);
-    finalized.add_block(&patch_label("conclusion"), conclusion);
+    finalized.add_block(&"main", prelude);
+    finalized.add_block(&"conclusion", conclusion);
     for (label, block) in prog.blocks {
-        let mut generated_block: Vec<Instruction<Arg>> = block
-            .into_iter()
-            .map(|instr| patch_label_instr(instr))
-            .collect();
+        let mut generated_block = block;
         if label == "start" {
             generated_block.push(Instruction::Jump {
-                label: patch_label("conclusion"),
+                label: "conclusion".to_owned(),
             })
         }
 
-        finalized.add_block(&patch_label(&label), generated_block);
+        finalized.add_block(&label, generated_block);
     }
     finalized
 }
@@ -57,7 +47,7 @@ fn generate_prelude(prog: &Program) -> Vec<Instruction<Arg>> {
         dest: Reg::Rbp.into(),
     });
     prelude.push(Instruction::Jump {
-        label: patch_label("start"),
+        label: "start".to_owned(),
     });
     prelude
 }
@@ -74,32 +64,10 @@ fn generate_conclusion(prog: &Program) -> Vec<Instruction<Arg>> {
             arg: callee_saved.clone().into(),
         });
     }
+    conc.push(Instruction::MovQ {
+        src: Arg::Immediate(0),
+        dest: Reg::Rax.into(),
+    });
     conc.push(Instruction::RetQ);
     conc
-}
-
-fn patch_label(label: &str) -> String {
-    if consts::OS == "macos" {
-        format!("_{label}")
-    } else {
-        label.to_owned()
-    }
-}
-
-fn patch_label_instr(instr: Instruction<Arg>) -> Instruction<Arg> {
-    match instr {
-        Instruction::AddQ { src, dest } => Instruction::AddQ { src, dest },
-        Instruction::SubQ { src, dest } => Instruction::SubQ { src, dest },
-        Instruction::NegQ { arg } => Instruction::NegQ { arg },
-        Instruction::MovQ { src, dest } => Instruction::MovQ { src, dest },
-        Instruction::PushQ { arg } => Instruction::PushQ { arg },
-        Instruction::PopQ { arg } => Instruction::PopQ { arg },
-        Instruction::CallQ { label } => Instruction::CallQ {
-            label: patch_label(&label),
-        },
-        Instruction::RetQ => Instruction::RetQ,
-        Instruction::Jump { label } => Instruction::Jump {
-            label: patch_label(&label),
-        },
-    }
 }
