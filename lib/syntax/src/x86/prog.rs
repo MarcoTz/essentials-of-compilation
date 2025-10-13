67d5e4633cdebed636ec725dcad40110adb1537e
MarcoTz <marco.tzschentke@gmail.com>
2025-10-13 11:23:39 +0200

    fixed generate prelude and conclusion test

--- lib/syntax/src/x86/prog.rs
+++ lib/syntax/src/x86/prog.rs
@@ -1,7 +1,7 @@
 use super::{Arg, Block, Reg};
 use std::{collections::HashSet, fmt};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone)]
 pub struct Program {
     pub blocks: Vec<Block<Arg>>,
     pub stack_space: u64,
@@ -18,11 +18,33 @@ impl Program {
     }
 }
 
+impl PartialEq for Program {
+    fn eq(&self, other: &Self) -> bool {
+        if self.stack_space != other.stack_space || (self.used_callee != other.used_callee) {
+            return false;
+        }
+        for block in self.blocks.iter() {
+            match other.blocks.iter().find(|bl| bl.label == block.label) {
+                None => return false,
+                Some(bl) => {
+                    if block != bl {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+}
+
+impl Eq for Program {}
+
 impl fmt::Display for Program {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, ".global main")?;
         for block in self.blocks.iter() {
             block.fmt(f)?;
+            writeln!(f)?;
         }
         Ok(())
     }
